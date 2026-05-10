c7272e2fc595c374bf52031b4f7301381bc9a5be
sankar <sankarnarayana910@gmail.com>
2026-05-10 22:28:04 +0530

    up

---
+++ one.py/basics.py
@@ -0,0 +1,90 @@
+# # python  is dynamic programming language it is used fro development purpose and  for machine learning purpose 
+
+# # invented by guido van rossum in 1991 
+
+# # programming is the process of creating of set of instructions that tell a computer to perform a task 
+
+# # coding is the step by step instructions that got computers to do what you want them to do 
+
+
+# # programiing = plan 
+# # coding = implementation 
+
+
+# # python is high level programming language 
+
+
+# complier converts the high level programming language to low level programming langugae such as machine code 
+
+# it will excute the code at once 
+# code execution is fast 
+# supoose if we have error in 3rd line of the code it will execute all lines of the code 
+# it is used for c, c++
+
+
+# interpreter : it checks the code line by line 
+# slow execution 
+# supoose u have 3 rd line of code it will stop their only 
+# pyhton , javascript, 
+
+
+
+# extension = the pyhton file is saveed as .Py
+
+# pvm- pyhton virtual machine 
+
+
+
+# pyhton is interpreted language object oreinted and high level programiimng language 
+
+# synax set of rules is called as a syntax 
+
+
+
+# mutable they can be chnaged after creation
+
+# dictonary, list, set 
+
+
+ 
+# immutable  they cant be changed after creaation 
+# int, float, string, tuple , bool, frozen set
+
+
+
+# variables is which stores the value of addres is called as variable 
+# it is defined with id 
+# it finds the memory address
+# we cant print  multiple variables with one value 
+
+
+# function :block of codde is called as a function 
+
+# module:collection of function is called a module 
+
+# package:collection of modules is called package 
+
+# library: collection of package is called as packages 
+
+# frame work a collection of everything is called frame work 
+
+
+
+
+# ide:integreted development environment is software for building application 
+# that combines common developers tools into single graphical user interface 
+
+
+# comments:they are ignored by the complier 
+# they are two types they are 
+# single line , mutilple line 
+# #               """"
+
+
+
+# variable is container which stores the value of addres 
+# in pyhton you dont want to decalre the type of varible 
+# a varible i screated at the moment we first assign the value to it 
+# we stored in memory of value it is called a address 
+
+ 
