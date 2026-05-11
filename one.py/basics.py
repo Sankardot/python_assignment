a5d2f678797b04285d6a3e30bcddc393eb1ac265
sankar <sankarnarayana910@gmail.com>
2026-05-11 12:41:05 +0530

    up

--- one.py/basics.py
+++ one.py/basics.py
@@ -64,7 +64,7 @@
 
 # package:collection of modules is called package 
 
-# library: collection of package is called as packages 
+# library: collection of package is called as library
 
 # frame work a collection of everything is called frame work 
 
@@ -88,3 +88,120 @@
 # we stored in memory of value it is called a address 
 
  
+#print("sankar")
+
+
+# sankar = 20
+# print(sankar)
+
+
+
+# a=b=c =100
+# print(b)
+
+# multiple varobels can pass throw one value in variables 
+
+
+# types of conversion in python
+
+# the are process of converting the value of one  datatype to antother data type is called a type conversion 
+# they are two types they are 
+
+# implict and explict 
+
+
+# implict type conversion : done automatically on pyhton 
+
+# no data losss
+
+# a = 1000#int 
+# print(float(a))
+
+
+
+# explict coversion:manaul uiing 
+# the user manually converts one data type to antother data type 
+# # data loss
+
+# a = 1000.0
+# print(int(a))
+
+
+
+# #inputfunction : the inputfunction is used to take the input from user during the exccution 
+# a=input("enter the name: ")
+# print(a)
+
+
+
+
+# operators is genarelly used to perform operations on the values and variables  is called operators 
+
+
+
+
+# control staments : control statements are used to change the flow of execution is calledd  control stamnts 
+# it is divides into three types 
+# conditonal staments , jumping staments , looping statments 
+
+# conditonal staments  sre also called as decison making stamnts  
+# if 
+# if else 
+# if elif 
+# nested if else 
+
+
+
+# condtional statemts are alsi called decison making statments 
+# we use those statments while we  want to excutte a block of code while the given is true 
+
+
+# num1 = int(input("enter the  first number:" ))
+# num2 = int(input("Enter the second number:"))
+# if num1>num2:
+#     print('greater than num1')
+# elif num2 > num1:
+#     print("greater than number2")
+# else:
+#     print("Both are equal number")
+
+
+
+# loopin statments are alos now as iterative staments if we want to excute a group 
+# of stament multiplt times then we should go for a looping kind of execution 
+# for loop and while loop 
+
+
+# while loop:
+#     repeats ststemts or group of statments while given condition is true it tests the conditon before execting the loop boody 
+
+
+# i = 1
+# while i < 10:
+#     print(i)
+#     i+=1
+    
+# for loop this type of loop executtes a code  block multiples time s abbrevites the code that manages the loop varible
+
+# in foor loop each and every element will be looped and it will be eneded at data ends 
+
+
+
+# for a in 'adaru':
+#     print(a)
+
+
+# for day in range (1,3):
+#     print("day", day)
+    
+#     water =1 
+#     while water <=2:
+#         print("drink water")
+#         water +=1
+        
+# range function : it is used genrare a sequence pf number 
+# synrtax(start , stop , step)
+
+
+for i in range(0,11,2):
+    print(i)
